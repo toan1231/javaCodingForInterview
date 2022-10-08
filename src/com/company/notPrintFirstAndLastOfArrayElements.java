395995a0190645eb98cf8c2d55365084bd968468
toan1231 <chauthai2003@gmail.com>
2022-10-07 21:06:51 -0400

    push2

--- src/com/company/notPrintFirstAndLastOfArrayElements.java
+++ src/com/company/notPrintFirstAndLastOfArrayElements.java
@@ -11,7 +11,7 @@ public class notPrintFirstAndLastOfArrayElements {
         for(int i=0;i<arr.length;i++){
             for(int j=0;j< arr.length;j++){
                 if(i!=j&&arr[i]+arr[j]==targetNum){
-
+                //toi muon merge
                 }
             }
         }
