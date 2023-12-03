c80b392c70bc93d34cae0ff496f4c632d71c3d7c
chaut <chauthai2003@gmail.com>
2023-12-02 22:33:49 -0500

    bbhhkknniip

--- src/com/company/RemoveDuplicate.java
+++ src/com/company/RemoveDuplicate.java
@@ -19,9 +19,10 @@ public class RemoveDuplicate {
 		Set<Character>set=new HashSet<Character>();
 		StringBuilder builder=new StringBuilder();
 		for(int i=0;i<str.length();i++) {
-			if(!set.contains(str.charAt(i))) {
-				set.add(str.charAt(i));
-				builder.append(str.charAt(i));
+			Character c=str.charAt(i);
+			if(!set.contains(c)) {
+				set.add(c);
+				builder.append(c);
 			}
 		}
 		
