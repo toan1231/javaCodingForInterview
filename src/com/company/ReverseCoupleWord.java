70e2a3fce2fbf70bda70f6311ff10b83d39d0104
chaut <chauthai2003@gmail.com>
2023-12-17 20:20:21 -0500

    ju

--- src/com/company/ReverseCoupleWord.java
+++ src/com/company/ReverseCoupleWord.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Testing {
+public class ReverseCoupleWord {
 	
 	   public static void main(String[] args) {
 		   String name="my name is chau he het thoi";
@@ -13,14 +13,14 @@ public class Testing {
 	   public static String method(String array){
 		 String newString=""; 
 		  String[]stringArray=array.split(" ");
-		 int index=0; 
-		 String[]stringNewArray=new String[stringArray.length];
 		  for(int i=stringArray.length-1;i>=0;i--) {
-			  stringNewArray[index]=stringArray[i];
-			  newString+= stringNewArray[index]+" ";
-			index++;  
-			//newString+= stringNewArray[index-1]+" ";
+			  newString+=stringArray[i]+" ";
 		  }
+
+
+		  
+
+
 		   
 		return newString;
 		   
