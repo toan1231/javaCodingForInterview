512874fc139d02211923e5664876e930c39b6ab8
chaut <chauthai2003@gmail.com>
2023-12-21 17:04:04 -0500

    jukfjdkfj

---
+++ src/com/company/Add_total_of_a_string_number.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Add_total_of_a_string_number {
+
+	public static void main(String[] args) {
+		
+		String name="12345678";
+		String[]name2=name.split("");
+		int total1=0;
+		int total2=0;
+		for(int i=0;i<name2.length;i++) {
+			int c=Integer.valueOf(name2[i]);
+			if(c%2==0) {
+				total1+=c;
+			}else {
+				total2+=c;
+			}
+		}
+		
+
+		System.out.println("total even is: "+total1);
+		System.out.println("total odd is: "+total2);
+		
+		
+
+}
+}
